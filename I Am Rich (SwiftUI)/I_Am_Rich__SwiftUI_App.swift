d6e40a126ce815e1eb42aa1f9ff3d6ad87b6549e
Gururaja M <gururajkumar161@gmail.com>
2025-06-28 19:40:02 +0530

    Initial Commit

---
+++ I Am Rich (SwiftUI)/I_Am_Rich__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich__SwiftUI_App.swift
+//  I Am Rich (SwiftUI)
+//
+//  Created by Gururaja M on 28/06/25.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
