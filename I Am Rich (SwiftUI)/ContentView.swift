396aecf8f891dbf1dec795da2948175708b4add9
Gururaja M <gururajkumar161@gmail.com>
2025-06-28 19:53:00 +0530

    Added ZStack the view

--- I Am Rich (SwiftUI)/ContentView.swift
+++ I Am Rich (SwiftUI)/ContentView.swift
@@ -9,13 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(.darkGray).ignoresSafeArea(edges: .all).opacity(0.7)
+            
         }
-        .padding()
     }
 }
 
